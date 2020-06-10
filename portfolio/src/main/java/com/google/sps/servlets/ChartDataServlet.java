d27b127bff72d3f5b9e11837051108611096ae2b
Valeria Lopez <val.lopez1011@gmail.com>
2020-06-10 15:47:09 +0000

    Addresses review comments

--- portfolio/src/main/java/com/google/sps/servlets/ChartDataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/ChartDataServlet.java
@@ -8,6 +8,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet that takes user votes and tallies them according to vote category.
+ * This is later visualized into a chart of the votes. 
+ */
 @WebServlet("/chart-data")
 public class ChartDataServlet extends HttpServlet {
     private HashMap<String, Integer> chartVotes = new HashMap<>();
@@ -23,14 +27,11 @@ public class ChartDataServlet extends HttpServlet {
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String uglyChoice = request.getParameter("ugly-choice");
-        System.out.println(uglyChoice);
-        int currentVotes;
+        final String uglyChoice = request.getParameter("ugly-choice");
+        int currentVotes = 0;
 
         if (chartVotes.containsKey(uglyChoice)) {
             currentVotes = chartVotes.get(uglyChoice);
-        } else {
-            currentVotes = 0;
         }
 
         chartVotes.put(uglyChoice, currentVotes + 1);
--- portfolio/src/main/webapp/mikey-images.js
+++ portfolio/src/main/webapp/mikey-images.js
@@ -1,32 +1,29 @@
 function displayMikeyImage() {
-    // const mikeyImages = ['mikey-1.jpg', 'mikey-2.jpg', 'mikey-3.jpg', 'mikey-4.jpg', 'mikey-5.jpg',
-    // 'mikey-6.jpg', 'mikey-7.jpg', 'mikey-8.jpg', 'mikey-9.jpg', 'mikey-10.jpg', 'mikey-11.jpg',
-    // 'mikey-12.jpg', 'mikey-13.jpg', 'mikey-14.jpg', 'mikey-15.jpg', 'mikey-16.jpg', 'mikey-17.jpg']
 
-    // const mikeyImage = mikeyImages[Math.floor(Math.random() * mikeyImages.length)];
-
-    const imageCaptions = {1: "He just... sat on our game board. He has a bed - I don't get it.", 
-    2: "He's just so tiny, everything else looks so big. Like a giant kingdom.",
-    3: "Sometimes we literally put him in a purse in order to bring him inside stores. Peep \
-    his little paw 🥺",
-    4: "I don't know why this is so funny. He just looks like Scrappy Doo.",
-    5: "Self-explanatory",
-    6: "He's so tiny, but he eats SO MUCH. He literally consumes things larger than his head \
-    - I'm just impressed... and confused.",
-    7: "I don't capture Mikey at his most photogenic moments.",
-    8: "Okay, he actually looks pretty cute in this one 💓",
-    9: "I feel like if you were to get a glimpse of Mikey's thoughts, it'd just be circus music... \
-    looping 24/7.",
-    10: "He looks judgemental in this picture, and he has no business judging other people.",
-    11: "This was made because he looked like that chicken meme where the chicken just ZOOMS.",
-    12: "This took so much effort and this was the best we could do.",
-    13: "🥺🥺",
-    14: "Not pictured. I am also wearing a matching hoodie.",
-    15: "We call this one Moot after that Poot Lovato meme. If you don't know what that is, don't worry \
-    about it. It won't help make sense either way.",
-    16: "My brother captured this absolutely DEMONIC picture of Mikey. It literally looks like \
-    a cursed image.",
-    17: "I seriously don't understand why this dog is so bad at pictures."}
+    const imageCaptions = {
+        1: "He just... sat on our game board. He has a bed - I don't get it.", 
+        2: "He's just so tiny, everything else looks so big. Like a giant kingdom.",
+        3: "Sometimes we literally put him in a purse in order to bring him inside stores. Peep \
+        his little paw 🥺",
+        4: "I don't know why this is so funny. He just looks like Scrappy Doo.",
+        5: "Self-explanatory",
+        6: "He's so tiny, but he eats SO MUCH. He literally consumes things larger than his head \
+        - I'm just impressed... and confused.",
+        7: "I don't capture Mikey at his most photogenic moments.",
+        8: "Okay, he actually looks pretty cute in this one 💓",
+        9: "I feel like if you were to get a glimpse of Mikey's thoughts, it'd just be circus music... \
+        looping 24/7.",
+        10: "He looks judgemental in this picture, and he has no business judging other people.",
+        11: "This was made because he looked like that chicken meme where the chicken just ZOOMS.",
+        12: "This took so much effort and this was the best we could do.",
+        13: "🥺🥺",
+        14: "Not pictured. I am also wearing a matching hoodie.",
+        15: "We call this one Moot after that Poot Lovato meme. If you don't know what that is, don't worry \
+        about it. It won't help make sense either way.",
+        16: "My brother captured this absolutely DEMONIC picture of Mikey. It literally looks like \
+        a cursed image.",
+        17: "I seriously don't understand why this dog is so bad at pictures."
+    }
     
     const randNum = Math.floor(Math.random()* 17 + 1);
 
