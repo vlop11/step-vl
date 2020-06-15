fea6de7bf715ac9249b22202481bc250a731eaaf
Valeria Lopez <val.lopez1011@gmail.com>
2020-06-15 21:13:52 +0000

    Adds query method body to pass initial tests for week 5 project.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,88 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        final long meetingDuration = request.getDuration(); // in minutes
+        Collection<String> requestAttendees = request.getAttendees();
+        Collection<String> optionalAttendees = request.getOptionalAttendees();
+
+        Collection<TimeRange> availableTimes = new ArrayList<>();
+        Collection<TimeRange> optionalTimes = new ArrayList<>();
+
+        if (meetingDuration > TimeRange.WHOLE_DAY.duration()) {
+            return availableTimes;
+        }
+        if (events.size() == 0) {
+            availableTimes.add(TimeRange.WHOLE_DAY);
+            return availableTimes;
+        }
+
+        System.out.println("Done------------------------------");
+        for (Event event : events) {
+            TimeRange when = event.getWhen();
+            Set<String> eventAttendees = event.getAttendees();
+
+            boolean hasNoCommonAttendees = Collections.disjoint(requestAttendees, eventAttendees);
+             
+            // if no event attendees need to go to requested meeting
+            if (hasNoCommonAttendees) {
+                if (availableTimes.size() == 0) {
+                    availableTimes.add(TimeRange.WHOLE_DAY);
+                }
+                continue;
+            }
+
+            // creates TimeRange available before and after event
+            TimeRange availableBefore = TimeRange.fromStartEnd(TimeRange.START_OF_DAY, when.start(), false);
+            TimeRange availableAfter = TimeRange.fromStartEnd(when.end(), TimeRange.END_OF_DAY, true);
+
+            // System.out.println("Before range: " + availableBefore + " & duration: " + availableBefore.duration());
+            // System.out.println("After range: " + availableAfter + " & duration: " + availableAfter.duration());
+            // System.out.println("Done------------------------------");
+            
+            availableTimes = returnOverlaps(availableBefore, availableAfter, availableTimes, meetingDuration);
+        }
+
+        return availableTimes;
+    }
+
+    private Collection<TimeRange> returnOverlaps(TimeRange before, TimeRange after, Collection<TimeRange> available, long duration) {
+        if (available.size() == 0) {
+            available.add(before);
+            available.add(after);
+            return available;
+        }
+
+        Collection<TimeRange> overlaps =  new ArrayList<>();
+        for (TimeRange range : available) {
+
+            if (range.overlaps(before)) {
+                int latestStart = range.start() < before.start() ? before.start() : range.start();
+                int earliestEnd = range.end() < before.end() ? range.end() : before.end();
+                TimeRange overlap = TimeRange.fromStartEnd(latestStart, earliestEnd, false);
+                if (overlap.duration() >= duration) {
+                    System.out.println("Overlap range: " + overlap);
+                    overlaps.add(overlap);
+                }
+            }
+
+            if (range.overlaps(after)) {
+                int latestStart = range.start() < after.start() ? after.start() : range.start();
+                int earliestEnd = range.start() < after.end() ? range.end() : after.start();
+                TimeRange overlap = TimeRange.fromStartEnd(latestStart, earliestEnd, false);
+                if (overlap.duration() >= duration) {
+                    System.out.println("Overlap range: " + overlap);
+                    overlaps.add(overlap);
+                }
+            }
+        }
+
+        return overlaps;
+    }
 }
